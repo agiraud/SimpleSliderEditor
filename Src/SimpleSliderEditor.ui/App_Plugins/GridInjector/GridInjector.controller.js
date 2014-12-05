5753fa2c4c5a95a6e6e6c593d901aef67b9554e8
antoine <antoine@lecoati.com>
2014-12-05 19:07:24 +0100

    First upload of the SimpleSlider Sample

---
+++ Src/SimpleSliderEditor.ui/App_Plugins/GridInjector/GridInjector.controller.js
@@ -0,0 +1,8 @@
+﻿angular.module("umbraco").controller("GridInjector.controller",
+    function ($scope, assetsService) {
+
+        if ($scope.model.config && $scope.model.config.cssBackendPath && $scope.model.config.cssBackendPath != "") {
+            assetsService.loadCss($scope.model.config.cssBackendPath);
+        }
+
+    });
---
+++ Src/SimpleSliderEditor.ui/App_Plugins/SimpleSliderEditor/SimpleSliderEditor.controller.js
@@ -0,0 +1,57 @@
+﻿angular.module("umbraco").controller("SimpleSliderEditor.controller",
+    function ($scope, assetsService, $http, dialogService, mediaHelper) {
+
+        var defaultItem = { name: "", summary: "", img: "", link: undefined };
+
+        if (!$scope.model.value) {
+            $scope.model.value = [];
+        }
+
+        if ($scope.model.value.length > 0) {
+            $scope.selected = $scope.model.value[0];
+        }
+
+        $scope.select = function (index) {
+            $scope.selected = index;
+        };
+
+        $scope.remove = function ($index) {
+            $scope.model.value.splice($index, 1);
+        };
+
+        $scope.add = function () {
+            $scope.model.value.splice($scope.model.value.length + 1, 0, angular.copy(defaultItem));
+            $scope.selected = $scope.model.value[$scope.model.value.length-1];
+        };
+
+        $scope.pick = function (slide) {
+            dialogService.mediaPicker({
+                multiPicker: false,
+                callback: function (data) {
+                    slide.img = mediaHelper.resolveFile(data, false);
+                }
+            });
+        };
+
+        $scope.pickContent = function (slide) {
+            dialogService.treePicker({
+                multiPicker: false,
+                section: "content",
+                treeAlias: "content",
+                callback: function (data) {
+                    slide.link = { name: data.name, id: data.id };
+                }
+            });
+        };
+
+        $scope.sortableOptions = {
+            handle: ".icon-navigation",
+            axis: "y",
+            delay: 150,
+            distance: 5
+        };
+
+        // Load css asset
+        assetsService.loadCss("/App_Plugins/SimpleSliderEditor/assets/SimpleSliderEditor.css");
+
+});
